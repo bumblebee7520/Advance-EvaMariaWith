cb9ea586074319e2892a995616ac71b2460e27ed
bumblebee7520 <115803864+bumblebee7520@users.noreply.github.com>
2023-01-28 14:15:32 +0530

    Update info.py

--- info.py
+++ info.py
@@ -22,7 +22,7 @@ PORT = environ.get("PORT", "8080")
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
-PICS = (environ.get('PICS', 'https://telegra.ph/file/b806ad314d0c415571bde.jpg')).split()
+PICS = (environ.get('PICS', 'https://telegra.ph/file/12ac341f206163756bfa8.jpg')).split()
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
@@ -80,7 +80,7 @@ DOWNLOAD_TEXT_NAME = "📥 HOW TO DOWNLOAD 📥"
 DOWNLOAD_TEXT_URL = "https://t.me/download_from_search_zone_bot"
 
    # Custom Caption Under Button #
-CAPTION_BUTTON = "Subscribe"
-CAPTION_BUTTON_URL = "https://youtube.com/@GreyMattersYat"
+CAPTION_BUTTON = "Join For Movie & "
+CAPTION_BUTTON_URL = "https://t.me/chat_official_sahil"
 
    # Auto Delete For Bot Sending Files #
